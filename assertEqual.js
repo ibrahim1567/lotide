7ae587ebfe2c62b670d32dbafadc2807ceb4a62e
Ibrahim Mohamed <ia.mohamed02@gmail.com>
2021-11-25 21:50:28 +0000

    Implementing Assert Equal

---
+++ assertEqual.js
@@ -0,0 +1,8 @@
+const assertEqual = function(actual, expected) {
+  if ( actual === expected ) {
+      console.log ("Assertion Passed:");
+  }
+  else {
+      console.log ("Assertion Failed");
+  }
+  };
