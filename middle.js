bb9dd3ca3278b7a66b957ec4b25402e70336969f
Ibrahim Mohamed <ia.mohamed02@gmail.com>
2021-11-26 02:38:18 +0000

    Implementing middle

---
+++ middle.js
@@ -0,0 +1,20 @@
+const middle = function(array) {
+  let mid = [];
+  if (array.length === 0 || array.length === 1 || array.length === 2) {
+    return mid;
+  } else {
+    const start = 0;
+    const end = array.length - 1;
+    const midIndex = Math.floor((start + end) / 2);
+    if (arr.length % 2 === 0) {
+      mid.push(array[midIndex]);
+      mid.push(array[midIndex + 1]);
+    } else {
+      mid.push(array[midIndex]);
+    }
+  }
+  return mid;
+};
+
+
+module.exports = middle;
