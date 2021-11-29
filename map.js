6ccf819628c4aafd9e6b545eb375fb505e47bf01
Ibrahim Mohamed <ia.mohamed02@gmail.com>
2021-11-29 02:15:49 +0000

    Implement Map

---
+++ map.js
@@ -0,0 +1,33 @@
+const eqArrays = function(actual, expected) {
+  let check = true;
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      check = false;
+    }
+  }
+
+  if (actual.length !== expected.length) {
+    check = false;
+  }
+  return check;
+};
+
+const assertArraysEqual = function(actual, expected) {
+
+  if (eqArrays(actual, expected) === true) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+// map function below
+const words = ["ground", "control", "to", "major", "tom"];
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
