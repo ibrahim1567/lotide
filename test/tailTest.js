a79f99cc8943764bd6ff32d4289ecb78fd5906db
Ibrahim Mohamed <ia.mohamed02@gmail.com>
2021-12-01 01:54:13 +0000

    tail Test

---
+++ test/tailTest.js
@@ -0,0 +1,11 @@
+const assert = require('chai').assert;
+const tail = require('../tail');
+
+describe("#tail", () => {
+  it("returns [Lighthouse, Labs] for [Hello, Lighthouse, Labs]", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+  it("returns [] for []", () => {
+    assert.deepEqual(tail([]), []);
+  });
+});
